d5a52655ad27adfc12ba9cbb4b074aa0bb60a6cb
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 20:14:29 +0300

    Update wow.php

--- wow.php
+++ wow.php
@@ -1,5 +1,5 @@
 <?php
-
+$text=$_GET['text'];
     if($text){
          if ($text == "/start") {
             $reply = "Welcome to the Bot $chat_id you a sign up";
@@ -8,11 +8,11 @@
         }elseif ($text == "Price") {
         }elseif ($text == "Wallet to pay") {
         }elseif ($text == "Exchenge") {
-            $answer = $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'How match?', 'reply_markup'=> $telegram->ForceReply(['force_reply' => true, 'selective'   => false])]);
-			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "exchange ".$answer." BTC"]);
+//            $answer = $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'How match?', 'reply_markup'=> $telegram->ForceReply(['force_reply' => true, 'selective'   => false])]);
+//			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "exchange ".$answer." BTC"]);
 		}elseif ($text == "Register") {
         }elseif ($text == "Balance") {
         }
     }else{
-    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+//    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
