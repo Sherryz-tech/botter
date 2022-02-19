adc519a46fbd7a4a7bcd86becbce8ffec45ebf03
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 20:10:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	$agora = new Agora;
     $telegram = new Api('5100938458:AAH71g8P2ROvg21YwKK6VLJ_02FPuk9yILY'); //Устанавливаем токен, полученный у BotFather
-    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    $result = '';//$telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
     
 	//print_r($result);
     $text = $result["message"]["text"]; //Текст сообщения
