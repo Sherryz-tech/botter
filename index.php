d518ecf8ca018959c7a6dcdf3167ae594956480f
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 15:08:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    $keyboard = [["Balance"], ["Wallet to pay"]]; //Клавиатура
+    $keyboard = [["Balance"], ["Wallet to pay"], ["Exchenge"]; //Клавиатура
 
     if($text){
          if ($text == "/start") {
@@ -28,13 +28,13 @@
 			$response = json_decode($agora->send_cmd("", "wallet-addr"));
 			$reply=$response->data->address;			
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply." amount? eg amount 0.5" ]);
-        }elseif (substr($text,0,6) == "amount") {
+        }elseif (substr($text,0,6) == "Excange") {
 			$a=trim(strstr($text, " "));
 			$a=(int)$a;
 			$response = json_decode($agora->send_cmd("", "wallet-addr"));
 			$reply=$response->data->address;
 			$amount=$a-rand(0,1);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply." amount to pay_".$amount." ".var_dump($amount)]);
+            $telegram->sendRequest([ 'chat_id' => $chat_id, 'text' => "Amount?"]);
         }elseif ($text == "Register") {
             $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
             $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
