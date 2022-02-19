a29331136a36d4325c88da84e179b4018838829a
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 12:22:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    $keyboard = [["Price"],["Balance"]]; //Клавиатура
+    $keyboard = [["Price"],["Balance"], ["Wallet to pay"]]; //Клавиатура
 
     if($text){
          if ($text == "/start") {
@@ -23,6 +23,10 @@
         }elseif ($text == "Price") {
 			$response = json_decode($agora->send_cmd("", "sell-bitcoins-online/USD/cryptocurrency"));
 			$reply=$response->data->ad_list[0]->data->temp_price;
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }elseif ($text == "Wallet to pay") {
+			$response = json_decode($agora->send_cmd("", "wallet-addr"));
+			$reply=$response->data->address;			
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }elseif ($text == "Register") {
             $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
