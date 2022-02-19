606588d69ce5bf5bbb2f40bc6205dd3b70ef7a10
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 12:36:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 			$a=(int)strstr($text, " ");
 			$response = json_decode($agora->send_cmd("", "wallet-addr"));
 			$reply=$response->data->address;			
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply." amount to pay ".  $a]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply." amount to pay ".  $a-5]);
         }elseif ($text == "Register") {
             $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
             $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
