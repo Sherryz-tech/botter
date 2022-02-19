4181f45afb5c93c501df2c21a910f09224b01976
Sherryz-tech <62069385+Sherryz-tech@users.noreply.github.com>
2022-02-19 15:30:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	
     include('vendor/autoload.php'); //Подключаем библиотеку
 	require_once("func/function_agora.php");
     use Telegram\Bot\Api; 
@@ -7,6 +8,7 @@
     $telegram = new Api('5100938458:AAH71g8P2ROvg21YwKK6VLJ_02FPuk9yILY'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
     
+	//print_r($result);
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
@@ -14,7 +16,7 @@
 
     if($text){
          if ($text == "/start") {
-            $reply = "Welcome to the Bot you a sign up";
+            $reply = "Welcome to the Bot $chat_id you a sign up";
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
         }elseif ($text == "/help") {
